4eb6ae7a57de03c52d7fbf764837d9b981473f6c
Radosław Kuzyk <re.kuzyk@gmail.com>
2025-06-14 21:16:08 +0200

    Open/create logfile in FileHandler.

--- modules/handlers.py
+++ modules/handlers.py
@@ -1,9 +1,14 @@
 from abc import ABC, abstractmethod
 from modules.log_entry import LogEntry
 from typing import List
+import os
 
 
 class Handler(ABC):
+    @abstractmethod
+    def __init__(self):
+        self._create_log_if_non_existent()
+
     @abstractmethod
     def persist_log(self, entry: LogEntry):
         pass
@@ -12,9 +17,26 @@ class Handler(ABC):
     def retrieve_all_logs(self) -> List[LogEntry]:
         pass
 
+    @abstractmethod
+    def _create_log_if_non_existent(self):
+        pass
+
 
 class FileHandler(Handler):
-    pass
+    def __init__(self, filename):
+        self.filename = filename
+        super(FileHandler, self).__init__()
+
+    def _create_log_if_non_existent(self):
+        if not os.path.exists(self.filename):
+            with open(self.filename, "w") as f:
+                f.write("")
+
+    def persist_log(self, entry: LogEntry):
+        pass
+
+    def retrieve_all_logs(self):
+        pass
 
 
 class JsonHandler(Handler):
--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -1,9 +1,40 @@
 from unittest import TestCase
-from handlers import FileHandler
+from unittest.mock import patch, mock_open, MagicMock
+from modules.handlers import FileHandler
 
 
-class FileAccessCreation(TestCase):
+@patch("builtins.open", new_callable=mock_open)
+@patch("os.path.exists", return_value=False)
+class FileHandlerFileAccessCreation(TestCase):
     """
-    
+    Tests for accessing/creating log csv files.
     """
+    @classmethod
+    def setUpClass(cls):
+        cls.file_path = "/file/to/open.txt"
+
+    def test_file_exists(self, mock_exists, _):
+        """
+        On receiving a filepath, __init__ should check if a file exists.
+        """
+        file_handler = FileHandler(self.file_path)
+        mock_exists.assert_called_once_with(self.file_path)
+
+    def test_opening_file(self, _, mock_open_file):
+        """
+        Should create an output file (if it doesn't exist).
+        """
+        FileHandler(self.file_path)
+        mock_open_file.assert_called_once_with(self.file_path, "w")
+
+    def test_writing_new_file(self, *rest):
+        # TODO: how to get the same result using patch as a decorator?
+        m = mock_open()
+        with patch('builtins.open', m):
+            FileHandler(self.file_path)
+        handle = m()
+        handle.write.assert_called_once_with("")
+
+
+class FileHandlerPersistingLogs(TestCase):
     pass
