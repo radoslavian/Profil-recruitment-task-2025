cb6522d9cb419e1ad9e842d97aa361ddca5d0e4e
Radosław Kuzyk <re.kuzyk@gmail.com>
2025-06-14 16:40:58 +0200

    Add an abstract Handler class and placeholders for inheriting classes.

---
+++ modules/handlers.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+from log_entry import LogEntry
+from typing import List
+
+
+class Handler(ABC):
+    @abstractmethod
+    def persist_log(self, entry: LogEntry):
+        pass
+
+    @abstractmethod
+    def retrieve_all_logs(self) -> List[LogEntry]:
+        pass
+
+
+class FileHandler(Handler):
+    pass
+
+
+class JsonHandler(Handler):
+    pass
+
+
+class CSVHandler(Handler):
+    pass
+
+
+class SQLiteHandler(Handler):
+    pass
