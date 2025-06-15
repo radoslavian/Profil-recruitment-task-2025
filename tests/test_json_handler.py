15995f24b54eab8e1ca33cc8c9410cdd089173e7
Radosław Kuzyk <re.kuzyk@gmail.com>
2025-06-15 15:20:13 +0200

    Update tests (+test names). Add test for file opening.

--- tests/test_json_handler.py
+++ tests/test_json_handler.py
@@ -3,16 +3,16 @@ from unittest.mock import patch, mock_open
 from modules.handlers import JsonHandler
 
 
+@patch("builtins.open", new_callable=mock_open)
 class OpeningCreatingLog(TestCase):
     @classmethod
     def setUpClass(cls):
         cls.path = "/path/to/file.json"
 
-    @patch("builtins.open", new_callable=mock_open)
-    def test_opening_file(self, mock_open_file):
+    def test_creating_file(self, mock_open_file):
         """
         Should check if the file exists and if doesn't - create one
-        and open for writing.
+        and open it for writing.
         """
         with patch("os.path.exists", return_value=False) as mock_exists:
             JsonHandler(self.path)
@@ -20,12 +20,20 @@ class OpeningCreatingLog(TestCase):
         mock_exists.assert_called_once_with(self.path)
         mock_open_file.assert_called_once_with("/path/to/file.json", "w")
 
-    def test_initializing_file(self):
+    @patch("os.path.exists", return_value=True)
+    def test_opening_file(self, mock_exists, mock_open_file):
+        """
+        Shouldn't open an existing output file.
+        """
+        mock_file_open = mock_open_file()
+        JsonHandler(self.path)
+        mock_file_open.write.assert_not_called()
+
+    def test_initializing_file(self, mock_open_file):
         """
         Should save an empty json array to a newly created file.
         """
-        mock_file_open = mock_open()
-        with patch("builtins.open", mock_file_open):
-            JsonHandler(self.path)
-        handle = mock_file_open()
+        handle = mock_open_file()
+
+        JsonHandler(self.path)
         handle.write.assert_called_once_with("[]")
