a30dac6c978c4ab9d09c299b95ef638d36420ab6
Radosław Kuzyk <re.kuzyk@gmail.com>
2025-06-20 12:36:38 +0200

    Remove LogEntry.uuid field property.

--- tests/test_log_entry.py
+++ tests/test_log_entry.py
@@ -1,6 +1,5 @@
 from unittest import TestCase
 import datetime
-import uuid
 from modules.log_entry import LogEntry, LogLevelValue
 
 
@@ -86,19 +85,6 @@ class LogEntryAttrEncapsulationConversion(TestCase):
 
     def test_dict_message(self):
         self.assertEqual(self.message, self.received_dict["message"])
-
-    def test_log_uuid(self):
-        """
-        LogEntry should be identified by uuidv4 (for easier set creation).
-        """
-        entry_test = LogEntry(date=self.date,
-                              level=self.log_level,
-                              msg=self.message)
-        uuid_from_string = uuid.UUID(entry_test.uuid, version=4)
-
-        self.assertIs(type(entry_test.uuid), str)
-        self.assertIs(type(uuid_from_string), uuid.UUID)
-        self.assertNotEqual(self.log_entry.uuid, entry_test.uuid)
 
 
 class LogEntryFromDict(TestCase):
